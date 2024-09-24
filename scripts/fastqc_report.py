18673ab29444744e202bd70d42f1c4f67c56031b
Scott G. Daniel <danielsg@email.chop.edu>
2024-09-24 16:31:29 -0400

    dont need report variable

--- scripts/fastqc_report.py
+++ scripts/fastqc_report.py
@@ -28,7 +28,7 @@ def parse_fastqc_quality(filename: str) -> pandas.DataFrame:
 
 with open(snakemake.log[0], "w") as l:
     quality_list = [
-        reports.parse_fastqc_quality(file) for file in snakemake.input.files
+        parse_fastqc_quality(file) for file in snakemake.input.files
     ]
     quality_list = [qr for qr in quality_list if qr is not None]
     quality_table = pandas.concat(quality_list, axis=1).transpose()
