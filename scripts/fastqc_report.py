3c45ab255c1fdc4c795f3a2511ed6a6265c1b1a3
Scott G. Daniel <danielsg@email.chop.edu>
2024-09-26 16:37:36 -0400

    added postQC FastQC step

--- scripts/fastqc_report.py
+++ scripts/fastqc_report.py
@@ -26,10 +26,10 @@ def parse_fastqc_quality(filename: str) -> pandas.DataFrame:
         sys.stderr.write(f"{filename} has no per-base sequence quality reports.")
         return None
 
-with open(snakemake.log[0], "w") as l:
-    quality_list = [
-        parse_fastqc_quality(file) for file in snakemake.input.files
-    ]
-    quality_list = [qr for qr in quality_list if qr is not None]
-    quality_table = pandas.concat(quality_list, axis=1).transpose()
-    quality_table.to_csv(snakemake.output[0], sep="\t", index_label="Samples")
+
+quality_list = [
+    parse_fastqc_quality(file) for file in snakemake.input.files
+]
+quality_list = [qr for qr in quality_list if qr is not None]
+quality_table = pandas.concat(quality_list, axis=1).transpose()
+quality_table.to_csv(snakemake.output[0], sep="\t", index_label="Samples")
